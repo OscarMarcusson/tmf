6771293f90796f6ced6d274912c57c1656fa2c46
Oscar Marcusson <62374937+OscarMarcusson@users.noreply.github.com>
2022-11-07 23:45:49 +0100

    Added variable creation & override

--- C#/Temporal Message Format/Parser.cs
+++ C#/Temporal Message Format/Parser.cs
@@ -24,5 +24,27 @@ namespace TemporalMessageFormat
 			// TODO:: Resolve properties via reflection, call AddVariable for each of them
 			return this;
 		}
+
+		public void SetValue(string id, string variable, string? value)
+		{
+			if (!Sections.TryGetValue(id, out var section))
+			{
+				section = new Section(id);
+				Sections[id] = section;
+			}
+
+			section.Variables[variable] = value;
+		}
+
+		public bool TryGetVariable(string id, string variable, out string? value)
+		{
+			if (!Sections.TryGetValue(id, out var section))
+			{
+				value = null;
+				return false;
+			}
+
+			return section.Variables.TryGetValue(variable, out value);
+		}
 	}
 }
--- C#/Tests/Variables.cs
+++ C#/Tests/Variables.cs
@@ -3,26 +3,47 @@ namespace Tests
 	[TestClass]
 	public class Variables
 	{
+		const string ID = "AS123-K92341";
+		const string FindError = "Could not find the variable";
+		const string WrongValueError = "The variable existed, but had the wrong value";
+
 		[TestMethod]
 		public void Add()
 		{
-			throw new NotImplementedException();
+			var parser = new Parser().AddVariable<string>("A");
+			parser.SetValue(ID, "A", "5");
+			Assert.IsTrue(parser.TryGetVariable(ID, "A", out var a), FindError);
+			Assert.AreEqual("5", a, WrongValueError);
 		}
 
 		[TestMethod]
 		public void Override()
 		{
-			throw new NotImplementedException();
+			var parser = new Parser().AddVariable<string>("A");
+			parser.SetValue(ID, "A", "5");
+			Assert.IsTrue(parser.TryGetVariable(ID, "A", out var a), FindError);
+			Assert.AreEqual("5", a, WrongValueError);
+
+			parser.SetValue(ID, "A", "9");
+			Assert.IsTrue(parser.TryGetVariable(ID, "A", out a), FindError);
+			Assert.AreEqual("9", a, WrongValueError);
 		}
 
 		[TestMethod]
 		public void On_value_changed_callbacks()
 		{
-			// TODO:: Ensure that it's only called when the value actually changes
-			//        "A = 5"
-			//        "A = 5"
-			//        Should NOT invoke this callback the second time
-			throw new NotImplementedException();
+			var numberOfCallbacks = 0;
+			var parser = new Parser().AddVariable<string>("A", x => numberOfCallbacks++);
+			parser.SetValue(ID, "A", "5");
+			parser.SetValue(ID, "A", "5");
+			parser.SetValue(ID, "A", "5");
+			parser.SetValue(ID, "A", "5");
+			parser.SetValue(ID, "A", "9");
+			parser.SetValue(ID, "A", "5");
+			Assert.AreEqual(3, numberOfCallbacks, "Expected 3 callbacks since most of the value setters are the same");
+
+			parser.AddVariable<string>("B", x => Assert.AreEqual("VALUE", x));
+			parser.SetValue(ID, "B", "VALUE");
 		}
 	}
 }
